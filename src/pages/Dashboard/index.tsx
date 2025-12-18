3b084920064520ca4e381033ac51388b9709f920
brooksjbr <brooksjbr@protonmail.ch>
2025-12-18 16:22:19 -0500

    feat: display user profile

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
+import UserProfile from '@/components/UserProfile/UserProfile'
+import { useSpotify, useCurrentUser } from '@/hooks/useSpotify'
+
 const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>
+  const { sdk } = useSpotify()
+  const { user, loading, error } = useCurrentUser(sdk)
+
+  if (loading) {
+    return <div className="p-6">Loading...</div>
+  }
+
+  if (error) {
+    return <div className="p-6 text-red-500">Error: {error}</div>
+  }
+
+  if (!user) {
+    return <div className="p-6">Please log in to view your profile.</div>
+  }
+
+  return (
+    <div className="p-6">
+      <UserProfile user={user} />
+    </div>
+  )
 }
 
 export default Dashboard
